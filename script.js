8b6c563ce1eb246044bf0a93ada57897131cea1a
Monserrat Aguilar <monserrat.aguilar.oyarzun@institutocomercialosorno.cl>
2024-04-03 13:34:33 -0400

    primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function aumentarLikes1() {
+    var content1= document.getElementById("meGusta1");
+    var like1 = parseInt(content1.innerHTML);
+    content1.innerHTML = like1 + 1 + "like(s)"
+}
+function aumentarLikes2() {
+    var content2= document.getElementById("meGusta2");
+    var like2 = parseInt(content2.innerHTML);
+    content2.innerHTML = like2 + 1 + "like(s)"
+}
+function aumentarLikes3() {
+    var content3= document.getElementById("meGusta3");
+    var like3 = parseInt(content3.innerHTML);
+    content3.innerHTML = like3 + 1 + "like(s)"
+}
